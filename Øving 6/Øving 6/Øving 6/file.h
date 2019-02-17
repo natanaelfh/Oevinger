ce7fc55060e79abb9bb995ff682d875e3edac63e
Natanael Ferreira Hjermann <natanaeh@ntnu.no>
2019-02-17 14:29:36 +0100

    ferdig med oppg 1 og 2

---
+++ Øving 6/Øving 6/Øving 6/file.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "std_lib_facilities.h"
+
+
+void writeFile();
+
+void readFile();
+
+void countFile();
---
+++ Øving 6/Øving 6/Øving 6/main.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+#include "file.h"
+
+int main(){
+
+	// Your code here
+	countFile();
+	return 0;
+}
